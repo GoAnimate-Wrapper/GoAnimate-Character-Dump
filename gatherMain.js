9b17ac4ee8c672a2e1210e8bfc417962ca79c817
1630228 <nastyahmede@gmail.com>
2019-11-25 06:43:05 +0000

    Modified gatherMain.js.

--- gatherMain.js
+++ gatherMain.js
@@ -9,6 +9,6 @@ const gt3 = process.env.GATHER_THRESH3;
 var charList = fs.readdirSync('characters');
 if (charList.length) {
 	var minFile = Number.parseInt(charList[0].substring(0, 9));
-	gather(minFile, gt1).then(() => gather(gt2, gt3));
+	gather(minFile, gt1).then(() => gather(gt2, gt3).then(() => gather(gt1, 0)));
 }
 else gather(gt2, gt3);
