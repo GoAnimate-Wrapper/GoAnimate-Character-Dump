1cbdf804bb4d3e06320a9a891f52585956263c20
Windows81 <nastyahmede@gmail.com>
2019-12-12 18:57:40 +0000

    Added files 330700000-330710999

--- gatherMain.js
+++ gatherMain.js
@@ -4,23 +4,15 @@ const gather = require('./gather');
 const rekt = require('./gitRekt');
 
 const args = process.argv.slice(2);
-const gt1 = args[1] || process.env.GATHER_THRESH1;
-const gt2 = args[2] || process.env.GATHER_THRESH2;
-const gt3 = args[3] || process.env.GATHER_THRESH3;
+const gt1 = process.env.GATHER_THRESH1;
+const gt2 = process.env.GATHER_THRESH2;
 
 
 console.log('Starting script.');
 rekt.init().then(() => {
 	var charList = fs.readdirSync('characters');
-	if (charList.length) {
-		if (args[0]) {
-			var minFile = Number.parseInt(charList[0].substring(0, 9));
-			gather(minFile, gt1).then(() => gather(gt2, gt3).then(() => gather(gt1, 0)));
-		}
-		else {
-			var maxFile = Number.parseInt(charList[charList.length - 1].substring(0, 9));
-			gather(maxFile, gt3);
-		}
-	}
+	if (charList.length)
+		if (args[0]) gather(Number.parseInt(args[1] || charList[0].substring(0, 9)), Number.parseInt(args[2] || gt1));
+		else gather(Number.parseInt(args[1] || charList[charList.length - 1].substring(0, 9)), Number.parseInt(args[2] || gt2));
 	else gather(gt2, gt3);
 });
