1b3aa41887b25058cff4c1e48734ba72f39488ca
Windows81 <nastyahmede@gmail.com>
2019-12-15 04:28:56 +0000

    Added files 254500000-254599999.

--- gatherMain.js
+++ gatherMain.js
@@ -3,17 +3,17 @@ const fs = require('fs');
 const gather = require('./gather');
 const rekt = require('./gitRekt');
 
-const args = process.argv.slice(2);
+const args = process.argv.slice(2).reverse();
 const startEnv = process.env.START_OVERRIDE || process.env.START;
 const endEnv = process.env.END_OVERRIDE || process.env.END;
 
 console.log('Starting script.');
 rekt.init().then(() => {
 	var charList = fs.readdirSync('characters').sort();
-	var start = args[1] || startEnv, end = args[2] || endEnv;
+	var start = args[1] || startEnv, end = args[0] || endEnv;
 
 	if (charList.length) {
-		switch (args[0]) {
+		switch (args[args.length - 1]) {
 			case 'up':
 				start = charList[charList.length - 1].substring(0, 9);
 				end = endEnv;
@@ -22,12 +22,6 @@ rekt.init().then(() => {
 				start = charList[0].substring(0, 9);
 				end = startEnv;
 				break;
-			case 'range':
-				start = charList[0].substring(0, 9);
-				end = startEnv;
-				break;
-			default:
-				args.map(v => Number.parseInt(v)).filter(isNaN).forEach(v => gather(v, v));
 		}
 	}
 	gather(Number.parseInt(start), Number.parseInt(end));
